9271d83697ba4e3c4b9712ab5bb363f38e8dac40
Ilya Burtsev <ilya.burtsev1986@gmail.com>
2021-11-26 19:07:10 +0500

    Настроил webpack для работы

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path =require('path')
+
+module.exports ={
+	output:{
+		filename:'[name]-[contenthash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer:{
+		port:8080
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			template:'./src/index.pug'
+		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({filename: '[name]-[contenthash].css'})
+	],
+	module:{
+		rules:[
+			{
+				test:/\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test:/\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			},
+			{
+				test:/\.pug$/,
+				use:'pug-loader'
+			},
+			{
+				test: /\.(png|jpg|svg|gif)$/i,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot)$/i,
+				type: 'asset/resource'
+			}
+		]
+	}
+}
