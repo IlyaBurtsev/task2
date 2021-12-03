e8b50ee22f197b39494e4d3b11a38a419c40ff34
Ilya Burtsev <ilya.burtsev1986@gmail.com>
2021-12-04 01:29:32 +0500

    Big refactoring. Cheked input_email/_date, button_checkbox/_radio

--- src/ui-kit/form-elements/form-elements.js
+++ src/ui-kit/form-elements/form-elements.js
@@ -2,8 +2,8 @@
 import '../_theme/ui-kit_theme_custom.scss'
 import './form-elements.scss'
 import'../input-field/input-field.scss'
-import'../input-field/_checkbox/input-field_checkbox.scss'
-import'../input-field/_radio/input-field_radio.scss'
+import'../button/_checkbox/button_checkbox.scss'
+import'../button/_radio/button_radio.scss'
 
 
 
