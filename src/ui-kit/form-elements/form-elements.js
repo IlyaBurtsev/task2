9240ecd015cb8cb6a62b67354feeea35f0e2b377
Ilya Burtsev <ilya.burtsev1986@gmail.com>
2021-12-03 21:24:17 +0500

    input-field-radio is complete.

--- src/ui-kit/form-elements/form-elements.js
+++ src/ui-kit/form-elements/form-elements.js
@@ -3,6 +3,7 @@ import '../_theme/ui-kit_theme_custom.scss'
 import './form-elements.scss'
 import'../input-field/input-field.scss'
 import'../input-field/_checkbox/input-field_checkbox.scss'
+import'../input-field/_radio/input-field_radio.scss'
 
 
 
