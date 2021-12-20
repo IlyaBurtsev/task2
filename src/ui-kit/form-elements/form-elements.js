7d64abf22036a1e81ba0f3b4d9cedc6cef8f7253
IlyaBurtsev <ilya.burtsev1986@gmail.com>
2021-12-20 22:42:46 +0500

    Refactor text-input

--- src/ui-kit/form-elements/form-elements.js
+++ src/ui-kit/form-elements/form-elements.js
@@ -1,3 +1,4 @@
+import 'jquery'
 import './form-elements.scss'
 import '../../ui-kit/_theme/ui-kit_theme_custom.scss'
 import '../input-field/input-field.js'
--- src/ui-kit/input-field/input-field.js
+++ src/ui-kit/input-field/input-field.js
@@ -1,2 +1,3 @@
 import './input-field.scss'
 import './_subscription/input-field_subscription.scss'
+
