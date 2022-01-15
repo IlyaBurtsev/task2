295956e0acbffe37ee58a26ec2fc33be088c9d78
IlyaBurtsev <ilya.burtsev1986@gmail.com>
2022-01-15 18:35:59 +0500

    Test changes.

--- src/ui-kit/form-elements/form-elements.js
+++ src/ui-kit/form-elements/form-elements.js
@@ -1,10 +1,11 @@
 import './form-elements.scss'
 import '../../ui-kit/_theme/ui-kit_theme_custom.scss'
-//import '../input/input'
+import '../input/input'
 import '../list/list'
 //import '../pagination/pagination'
 //import '../slider/slider'
 import '../room-info/room-info'
+
 
 
 
