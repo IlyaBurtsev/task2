70595204faefb77c364d2d3394d7b0852dbf975d
IlyaBurtsev <ilya.burtsev1986@gmail.com>
2022-10-27 10:13:46 +0500

    PP: Headers&Footers.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,4 @@
 import './footer.scss'
-import './_mobile/footer_mobile.scss'
 import '../../components/links-block/links-block'
 import '../../components/input-field/input-field'
 
@@ -7,7 +6,6 @@ import { addClass, getElement} from '../../utils/utils';
 
 const initFooterMobileDemo = (bindElement) => {
 	const mobileFooter = getElement('.footer__container', bindElement);
-	console.log(mobileFooter)
 	const footerWidth = mobileFooter.offsetWidth;
 	if (footerWidth <1100) {
 		addClass(mobileFooter, 'footer__container_mobile-demo')
